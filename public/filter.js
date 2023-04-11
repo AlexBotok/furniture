117698809909ceb8fbb6cc31f90b23e7bb919e6f
bot4ka <47043242+AlexBotok@users.noreply.github.com>
2023-04-11 18:27:30 +0300

    filter 60/40

--- public/filter.js
+++ public/filter.js
@@ -48,3 +48,28 @@ checkboxes.forEach(function (checkbox) {
         }
     });
 });
+
+// Получаем все чекбоксы брендов
+const brandCheckboxes = document.querySelectorAll('.brand');
+
+// Получаем все товары
+const products = document.querySelectorAll('.goods1');
+
+// Добавляем обработчики событий на чекбоксы
+brandCheckboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('click', function() {
+    // Получаем значение выбранного чекбокса
+    const brand = this.innerText;
+    console.log(brand)
+    
+    // Перебираем все товары и скрываем те, которые не относятся к выбранному бренду
+    products.forEach(function(product) {
+      const productBrand = product.querySelector('.brand').innerText;
+      if (productBrand !== brand) {
+        product.style.display = 'none';
+      } else {
+        product.style.display = 'flex';
+      }
+    });
+  });
+});
